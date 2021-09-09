bc2fd3717897e6be6db90ae994ae6ebdf2515876
Raman.Naumovich <naumovichhh@gmail.com>
2021-09-10 00:23:43 +0300

    Forgotten files added

---
+++ TemperatureViewer/Models/MeasurementDateGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TemperatureViewer.Models
+{
+    public class MeasurementDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? MeasurementDate { get; set; }
+
+        public int MeasurementsCount { get; set; }
+    }
+}
