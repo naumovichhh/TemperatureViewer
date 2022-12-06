11d61fe3326d5980991fcb37561d9b43e7740435
Roman Naumovich <naumovichhh@gmail.com>
2022-12-06 13:20:13 +0300

    Authorization for temperatures and history view was added. Title part and some headers changed.

--- TemperatureViewer/Controllers/HomeController.cs
+++ TemperatureViewer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
@@ -16,6 +17,7 @@ using TemperatureViewer.Models.ViewModels;
 
 namespace TemperatureViewer.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
