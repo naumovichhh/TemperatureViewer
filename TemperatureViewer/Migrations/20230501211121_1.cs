e30bc0563c93ff79b5b51852c6631646c067ad7c
Raman.Naumovich <naumovichhh@gmail.com>
2023-05-02 00:27:02 +0300

    Migrations created

---
+++ TemperatureViewer/Migrations/20230501211121_1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TemperatureViewer.Migrations
+{
+    public partial class _1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Observers",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Observers", x => x.Email);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Thresholds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    P1 = table.Column<int>(type: "int", nullable: false),
+                    P2 = table.Column<int>(type: "int", nullable: false),
+                    P3 = table.Column<int>(type: "int", nullable: false),
+                    P4 = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Thresholds", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sensors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Uri = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: true),
+                    WasDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    XPath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ThresholdId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sensors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sensors_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Sensors_Thresholds_ThresholdId",
+                        column: x => x.ThresholdId,
+                        principalTable: "Thresholds",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Measurements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SensorId = table.Column<int>(type: "int", nullable: false),
+                    MeasurementTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Temperature = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Measurements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Measurements_Sensors_SensorId",
+                        column: x => x.SensorId,
+                        principalTable: "Sensors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ObserverSensor",
+                columns: table => new
+                {
+                    ObserversEmail = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    SensorsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ObserverSensor", x => new { x.ObserversEmail, x.SensorsId });
+                    table.ForeignKey(
+                        name: "FK_ObserverSensor_Observers_ObserversEmail",
+                        column: x => x.ObserversEmail,
+                        principalTable: "Observers",
+                        principalColumn: "Email",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ObserverSensor_Sensors_SensorsId",
+                        column: x => x.SensorsId,
+                        principalTable: "Sensors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Measurements_SensorId",
+                table: "Measurements",
+                column: "SensorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ObserverSensor_SensorsId",
+                table: "ObserverSensor",
+                column: "SensorsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sensors_LocationId",
+                table: "Sensors",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sensors_ThresholdId",
+                table: "Sensors",
+                column: "ThresholdId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Measurements");
+
+            migrationBuilder.DropTable(
+                name: "ObserverSensor");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Observers");
+
+            migrationBuilder.DropTable(
+                name: "Sensors");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Thresholds");
+        }
+    }
+}
---
+++ TemperatureViewer/Migrations/20230501212015_2.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TemperatureViewer.Migrations
+{
+    public partial class _2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Measurements");
+
+            migrationBuilder.RenameColumn(
+                name: "WasDeleted",
+                table: "Sensors",
+                newName: "WasDisabled");
+
+            migrationBuilder.CreateTable(
+                name: "SensorUser",
+                columns: table => new
+                {
+                    SensorsId = table.Column<int>(type: "int", nullable: false),
+                    UsersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorUser", x => new { x.SensorsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_SensorUser_Sensors_SensorsId",
+                        column: x => x.SensorsId,
+                        principalTable: "Sensors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SensorUser_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Values",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SensorId = table.Column<int>(type: "int", nullable: false),
+                    MeasurementTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Temperature = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Values", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Values_Sensors_SensorId",
+                        column: x => x.SensorId,
+                        principalTable: "Sensors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorUser_UsersId",
+                table: "SensorUser",
+                column: "UsersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Values_SensorId",
+                table: "Values",
+                column: "SensorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SensorUser");
+
+            migrationBuilder.DropTable(
+                name: "Values");
+
+            migrationBuilder.RenameColumn(
+                name: "WasDisabled",
+                table: "Sensors",
+                newName: "WasDeleted");
+
+            migrationBuilder.CreateTable(
+                name: "Measurements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MeasurementTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SensorId = table.Column<int>(type: "int", nullable: false),
+                    Temperature = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Measurements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Measurements_Sensors_SensorId",
+                        column: x => x.SensorId,
+                        principalTable: "Sensors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Measurements_SensorId",
+                table: "Measurements",
+                column: "SensorId");
+        }
+    }
+}
