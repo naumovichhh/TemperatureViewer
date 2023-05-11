44d30fdd37f9585091ead3c8ecca5618b67b649e
Roman Naumovich <naumovichhh@gmail.com>
2023-05-11 16:58:47 +0300

    Grey indication of absent sensor value was started

--- TemperatureViewer/wwwroot/js/index.js
+++ TemperatureViewer/wwwroot/js/index.js
@@ -1,8 +1,20 @@
 ﻿$(document).ready(function () {
     $(".therm-li").each((i, e) => {
-        let str = $(e).find(".temp-value").contents().text();
-        let value = Number(str.replace(",", "."));
         let r = 0, g = brightness, b = 0;
+        let str = $(e).find(".temp-value").contents().text();
+        if (str == "") {
+            e.nextSibling.textContent = "";
+            r = g = b = brightness;
+            let thermColor = `rgb(${r}, ${g}, ${b})`;
+            let liColor = `rgb(${r + 110}, ${g + 110}, ${b + 110})`
+            $(e).find(".therm").each((i, t) => {
+                t.style.backgroundColor = thermColor;
+            })
+            e.style.backgroundColor = liColor;
+            return;
+        }
+
+        let value = Number(str.replace(",", "."));
 
         if (value >= thresholds[i][3] || value <= thresholds[i][0]) {
             g = 0;
